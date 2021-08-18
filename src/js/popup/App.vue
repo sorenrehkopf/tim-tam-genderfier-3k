ed2c2b589bfad3a25ea6b6e74daf01a831f26be0
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2021-08-18 13:01:59 +0200

    gender list updating functional

--- src/js/popup/App.vue
+++ src/js/popup/App.vue
@@ -1,48 +1,69 @@
 <template>
 	<div class="app-container p-5">
 		<h1 class="title is-4 is-underlined">Gender Extender!</h1>
-		<label class="label">Genders</label>
-		<div>
-			<div v-for="gender in genders">
+		<label class="label mb-1">Genders</label>
+		<p class="help mb-2">What genders would you like to see reflected on forms?</p>
+
+		<div class="tags">
+			<span v-for="gender in genders" class="tag m-1" :key="gender">
 				{{gender}}
-			</div>
+				<button class="delete is-small" @click="() => removeGender(gender)"></button>
+			</span>
 		</div>
-		<div class="field has-addons">
+
+		<div class="field has-addons mt-">
 			<div class="control">
-				<input type="text" placeholder="An exciting new gender!" class="input" />
+				<input type="text" placeholder="An exciting new gender!" class="input" v-model="newGender"/>
 			</div>
 
 			<div class="control">
-		    <a class="button is-info">
+		    <a class="button is-info" @click="addGender">
 		      +
 		    </a>
 		  </div>
 		</div>
-		<p>What genders would you like to see reflected on forms?</p>
 	</div>
 </template>
 
 <script type="text/javascript">
-	import { onBeforeMount } from 'vue';
 	import defaultGenders from './defaultGenders'
 
 	export default {
-		computed: {
-			genders() {
-				return localStorage.getItem('genders').split(',');
+		data: () => ({
+			newGender: '',
+			genders: localStorage.getItem('genders').split(','),
+		}),
+
+		watch: {
+			genders(newGenders) {
+				localStorage.setItem('genders', newGenders.join(','));
 			}
 		},
+
+		methods: {
+			addGender() {
+				const { genders, newGender } = this;
+
+				this.genders = [...genders, newGender];
+				this.newGender = '';
+			},
+
+			removeGender(targetGender) {
+				const { genders } = this;
+
+				this.genders = genders.filter(gender => gender !== targetGender);
+			}
+		},
+
 		setup() {
-			onBeforeMount(() => {
-				if (!localStorage.getItem('genders')) {
-					localStorage.setItem('genders', defaultGenders)
-				}
-			})
+			if (!localStorage.getItem('genders')) {
+				localStorage.setItem('genders', defaultGenders)
+			}
 		},
 	};
 </script>
 
 <style lang="scss">
-	.app-container {
+	.tags-container {
 	}
 </style>
