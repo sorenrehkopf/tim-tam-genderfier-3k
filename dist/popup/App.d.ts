37730973d6308dcc41fa989ffb2b65677ef23b82
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2022-01-06 14:03:07 -0800

    basic styling and adding

--- dist/popup/App.d.ts
+++ dist/popup/App.d.ts
@@ -1,11 +1,13 @@
 import * as React from "react";
 declare type AppState = {
     labelOps: string[][];
+    newLabel: string;
 };
 export default class App extends React.Component<{}, AppState> {
     state: AppState;
     removeLabelOp: (targetOp: string[]) => void;
-    addLabelOp: () => boolean;
+    addLabelOp: (event: React.FormEvent<HTMLFormElement>) => void;
+    handleNewLabelChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     render(): JSX.Element;
 }
 export {};
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,24 +1,28 @@
 import * as React from "react";
 import { getLabelOps } from '../label-ops';
 import {
+	Button,
 	Divider,
 	Icon,
 	Input,
+	InputAdornment,
 	List,
 	ListItem,
 	ListItemIcon,
 	ListItemText,
 	Typography
 } from '@mui/material';
-import { Delete } from '@mui/icons-material';
+import { Add, Delete } from '@mui/icons-material';
 
 type AppState = {
-	labelOps: string[][]
+	labelOps: string[][],
+	newLabel: string
 };
 
 export default class App extends React.Component <{}, AppState> {
 	state: AppState = {
-		labelOps: getLabelOps()
+		labelOps: getLabelOps(),
+		newLabel: ''
 	};
 
 	removeLabelOp = (targetOp: string[]): void => {
@@ -31,34 +35,70 @@ export default class App extends React.Component <{}, AppState> {
 		})
 	};
 
-	addLabelOp = (): boolean => {
-		alert('yeah!!');
+	addLabelOp = (event: React.FormEvent<HTMLFormElement>): void => {
+		const { state: { newLabel, labelOps } } = this;
 
-		return false;
+		event.preventDefault();
+
+		if (/,/g.test(newLabel)) {
+			const [op1, op2] = newLabel.split(',');
+			const newLabelOps = [...labelOps, [op1, op2]];
+
+			this.setState({
+				labelOps: newLabelOps,
+				newLabel: ''
+			});
+		}
+	};
+
+	handleNewLabelChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		this.setState({
+			newLabel: event.target.value
+		});
 	};
 
   render() {
-  	const { addLabelOp, removeLabelOp, state: { labelOps } } = this;
+  	const { addLabelOp, handleNewLabelChange, removeLabelOp, state: { labelOps, newLabel } } = this;
 
     return (
       <div>
-        <Typography variant="h4">Tim Tam Genderfier 3k</Typography>
-        <Divider />
-        <Typography variant="h5">Label pairs</Typography>
+        <Typography variant="h5" mb={2}>Tim Tam Genderfier 3k</Typography>
+        <Typography variant="subtitle1">Spectrum Labels</Typography>
+        <Divider/>
+
         <List>
         {
         	labelOps.map((op: string[]): JSX.Element => (
         		<ListItem key={op[0]}>
         			<ListItemText>{op.join(', ')}</ListItemText>
         			<ListItemIcon onClick={(): void => removeLabelOp(op)}>
-        				<Delete></Delete>
+        				<Delete/>
         			</ListItemIcon>
         		</ListItem>
         	))
         }
         </List>
-        <form onSubmit={addLabelOp}>
-        	<Input placeholder="New,Genders!" />
+
+        <Typography mt={1} variant="subtitle1">Add New</Typography>
+        <Divider/>
+
+        <form
+        	onSubmit={addLabelOp}
+      		style={{ padding: "0px 30px 0px 16px", margin: "16px 0px"}}
+        >
+      		<Input
+      			fullWidth={true}
+      			placeholder="New,Genders!"
+      			onChange={handleNewLabelChange}
+      			value={newLabel}
+      			endAdornment={
+      				<InputAdornment position="end">
+      					<Button type="submit">
+      						<Add/>
+      					</Button>
+      				</InputAdornment>
+      				}
+      			/>
         </form>
       </div>
     );
